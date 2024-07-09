b233bff2c5c849fe04c0c5b99ed7d6d47963fe51
Herbert Abdillah <herbert.abdillah@gmail.com>
2024-07-10 00:19:22 +0700

    fix explorer

--- up.sh
+++ up.sh
@@ -4,6 +4,9 @@ export ROOT_PATH="${ROOT_PATH:-$HOME/workspaces/skripsi}"
 export NETWORK_PATH="${NETWORK_PATH:-$ROOT_PATH/fabric-samples/test-network}"
 
 cd ${ROOT_PATH}/explorer
+docker compose down
+docker volume rm explorer_pgdata || true
+docker volume rm explorer_walletstore || true
 docker compose up -d
 
 cd ${ROOT_PATH}/skripsi-nginx
