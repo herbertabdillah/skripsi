6ea2cc733118b8c4e2efd351b6cf141dc24d68bb
Herbert Abdillah <herbert.abdillah@gmail.com>
2024-07-14 12:59:24 +0700

    wip

---
+++ ais/lib/tasks/seed.rake
@@ -0,0 +1,7 @@
+namespace :seed do
+  desc "TODO"
+
+  task :fill_semester_and_sync_until, [:start_semester, :end_semester] do |t, args|
+    Factory.fill_semester_and_sync_until(args[:start_semester].to_i,args[:end_semester].to_i)
+  end
+end
--- reset-db.sh
+++ reset-db.sh
@@ -4,5 +4,4 @@ cd ais
 bin/spring stop || true
 docker compose up -d postgres
 docker compose stop app worker
-# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 rails db:drop db:create db:migrate
-rails db:drop db:create db:migrate db:seed
+docker compose run --rm  app bash -c "rails db:drop db:create db:migrate db:seed DISABLE_DATABASE_ENVIRONMENT_CHECK=1"
